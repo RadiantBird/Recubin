4675a96b4519bcc86a8aa63875710b975b38c41c
RadiantBird <pengyicesky@gmail.com>
2026-05-30 11:52:03 +0900

    Goodbye unwanted files!

--- temp_build/deps_md/build_yaml_project_source/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Ryarta/Documents/Recubin/temp_build/yaml-cpp
-# Build directory: C:/Users/Ryarta/Documents/Recubin/temp_build/deps_md/build_yaml_project_source
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
